2b2cec9818d0e194625bc696ede77441f3aefd81
Ariel Alves <arielalves9@gmail.com>
2022-06-13 20:38:02 -0300

    add constructor and function in class Pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,16 +2,18 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Alves"
-
     var cpf: String = "123.123.123.12"
     private set
 
+    constructor()
+    fun pessoaInfo() = "$nome , $cpf"
 }
 fun main(){
 
     val alves = Pessoa()
-    
+
     println(alves.nome)
     println(alves.cpf)
+    println(alves.pessoaInfo())
 
 }
